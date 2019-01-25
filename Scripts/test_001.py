ddb7053aeefa270e08b62664b7a7ad287689d069
zuozuojinjin123 <1454718526@qq.com>
2019-01-25 21:42:54 +0800

    修改代码

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -1,8 +1,11 @@
-class Test_01:
-    def test_001(self):
-        print("test_001")
+import allure
+class Test_001:
+    @allure.step(title="我是测试步骤1")
+    def test_001_01(self):
+        print("test_001_01步骤")
         assert True
 
-    def test_002(self):
-        print("test_002")
+    @allure.step(title="test_002_02步骤")
+    def test_001_02(self):
+        print("test_002_02步骤")
         assert False
